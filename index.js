36bd0ff91d7f194361be7ac6f711bbea0eb8c4ae
damndelion <106752754+damndelion@users.noreply.github.com>
2022-12-19 17:10:13 +0600

    Update of main page

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-window.addEventListener('scroll', handleScroll);
 
+window.addEventListener('scroll', handleScroll);
 function handleScroll() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;  
     var scrolled = (winScroll / height) * 100;
-  
+
     document.getElementById("progressBar").style.width = scrolled + "%";
 }
-
 $( ".scroll" ).scroll( 300 );
